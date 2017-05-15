9babc168e592eaf98a163b3d0c97cbe6be078659
shishir thapa <shishirthapa1@gmail.com>
2017-05-15 22:00:44 +0545

    console print without main works in java 6 or less only

---
+++ ConsolePrint/src/consoleprint/ConsolePrint.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consoleprint;
+
+/**
+ *
+ * @author ccr
+ */
+public final class ConsolePrint {
+    static {
+        System.out.println("Yeah, I printed this text without executing main function. :D");
+        System.exit(0);
+    }
+}
